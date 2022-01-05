8ef618550f348b4f1d6708442c262ffbd696672a
Aliona990220 <aliona.havela@gmail.com>
2022-01-05 13:45:01 +0200

    login page

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 
@@ -6,5 +8,38 @@ public class LoginPage {
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+    }
+    private By userName = By.xpath("//input[@id='login_field']");
+    private By passWord = By.xpath("//input[@id='login_field']");
+    private By siginInButtonToo = By.xpath("//a[@class='btn btn-primary btn-block js-sign-in-button']");
+    private By heading = By.xpath("//div[@class='auth-form-header p-0']/h1");
+    private By error = By.xpath("//div[@class='flash flash-full flash-error']");
+
+    public LoginPage typUserName(String username){
+        WebElement usname = driver.findElement(userName);
+        usname.click();
+        usname.clear();
+        usname.sendKeys("username");
+        return this;
+    }
+    public LoginPage typpassWord(String password){
+        WebElement pasW = driver.findElement(passWord);
+        pasW.click();
+        pasW.clear();
+        pasW.sendKeys("password");
+        return this;
+    }
+    public LoginPage incorectLogin(String username, String password) {
+        this.typUserName(username);
+        this.typpassWord(password);
+        driver.findElement(siginInButtonToo).click();
+        return new LoginPage(driver);
+    }
+    public String getHeadingText(){
+      return driver.findElement(heading).getText();
+
+    }
+    public String getErrorText(){
+        return driver.findElement(error).getText();
     }
 }
--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -10,9 +10,7 @@ public class MainPage {
     }
     private By siginInButton = By.xpath("//a[@class='HeaderMenu-link flex-shrink-0 no-underline']");
     private By siginUpButton = By.xpath("//a[@class='HeaderMenu-link flex-shrink-0 d-inline-block no-underline border color-border-default rounded px-2 py-1']");
-//    private By loginButton = By.xpath("//input[@id='login_field']");
-//    private By passwordButton = By.xpath("//input[@id='login_field']");
-//    private By siginInButtonToo = By.xpath("//a[@class='btn btn-primary btn-block js-sign-in-button']");
+
 
     public LoginPage clikSiginIn() {
         driver.findElement(siginInButton).click();
@@ -25,7 +23,7 @@ public class MainPage {
 
     }
 
-    
+
 
 
 }
