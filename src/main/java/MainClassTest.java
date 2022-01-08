bdf11313ebbb4bca0e52df204b8a52c7ce666e93
Aliona990220 <aliona.havela@gmail.com>
2022-01-08 18:05:43 +0200

    annotations JU

---
+++ src/main/java/MainClassTest.java
@@ -0,0 +1,34 @@
+import org.junit.*;
+
+public class MainClassTest {
+    @BeforeClass
+    public void beforeClassMetod() {
+
+    }
+    @Before
+    public void setUp(){
+
+    }
+    @After
+    public void tearDown(){
+
+    }
+    @Test
+    public void metod1(){
+
+    }
+    @Test
+    public void metod2(){
+
+    }
+    @Test
+    public void metod3(){
+
+    }
+
+    @AfterClass
+    public void afterClass(){
+
+
+    }
+}
